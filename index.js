060161e6e893c2cbb9f7928116218aceff28ed1a
Danielle Moylan <dmmoylan@sau29.org>
2022-08-20 18:09:25 -0400

    final revisions and project file

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
+// Create order class
 class Order {
-    constructor(name) {
+    constructor(name, color, quantity) {
         this.name = name;
-        this.bottles = [];
-    }
-
-    addBottle(color, quantity) {
-        this.bottles.push(new Bottle(color, quantity));
-    }
-}
-
-class Bottle {
-    constructor(color, quantity) {
         this.color = color;
         this.quantity = quantity;
     }
@@ -18,7 +9,7 @@ class Bottle {
 
 // Interact with API
 class OrderService {
-    static url = 'https://630105c89a1035c7f8fc9ddd.mockapi.io/api/orders';
+    static url = 'https://ancient-taiga-31359.herokuapp.com/api/houses';
 
     static getAllOrders() {
         return $.get(this.url);
@@ -58,8 +49,8 @@ class DOMManager {
         OrderService.getAllOrders().then(orders => this.render(orders));
     }
 
-    static createOrder(name) {
-        OrderService.createOrder(new Order(name))
+    static createOrder(name, color, quantity) {
+        OrderService.createOrder(new Order(name, color, quantity))
             .then(() => {
                 return OrderService.getAllOrders();
             })
@@ -74,77 +65,29 @@ class DOMManager {
         .then((orders) => this.render(orders));
     }
 
-    static addBottle(id) {
-        for (let order of this.orders) {
-            if(order._id == id) {
-                order.bottles.push(new Bottle($(`#${order._id}-bottle-color`).val(), $(`#${order._id}-bottle-quantity`).val()));
-                OrderService.updateOrder(order)
-                    .then(() => {
-                        return OrderService.getAllOrders()
-                    })
-                    .then((orders) => this.render(orders));
-                    }
-                }
-            }
-
-    static deleteBottle(orderId, bottleId) {
-        for (let order of this.orders) {
-            if (order._id == orderId) {
-                for (let bottle of order.bottles) {
-                    if (bottle._id == bottleId) {
-                        order.bottles.splice(order.bottles.indexOf(bottle), 1);
-                        OrderService.updateOrder(order)
-                            .then(() => {
-                                return OrderService.getAllOrders();
-                            })
-                            .then((orders) => this.render(orders));
-                    }
-                }
-            }
-        }
-    }
-
     static render(orders) {
         this.orders = orders;
-        $('#inventory').empty();
+        $('#orders-list').empty();
         for(let order of orders) {
-            $('#inventory').append(
+            $('#orders-list').prepend(
                 `<div id="${order._id}" class="card">
                     <div class="card-header">
                         <h4>${order.name}</h4>
+                        <p>Color: ${order.color}, Quantity: ${order.quantity}</p>
                         <button class="btn btn-warning" onclick="DOMManager.deleteOrder('${order._id}')">Delete</button>
-                    </div>
-                    <div class="card-body">
-                        <div class="card">
-                            <div class="row">
-                                <div class="col-sm">
-                                    <input type="text" id="${order._id}-bottle-color" class="form-control" placeholder="Bottle Color">
-                                </div>
-                                <div class="col-sm">
-                                    <input type="text" id="${order._id}-bottle-quantity" class="form-control" placeholder="Quantity">
-                                </div>
-                            </div>
-                            <button id="${order._id}-new-bottle" onclick="DOMManager.addBottle('${order._id}')" class="btn btn-primary form-control">Add</button>
-                        </div>
-                    </div>
+                    </div>                
                 </div><br>`
             );
-            for (let bottle of order.bottles) {
-                $(`#${order._id}`).find('.card-body').prepend(
-                    `<p>
-                        <span id="color-${bottle._id}"><strong>Color: </strong> ${bottle.color}</span>
-                        <span id="quantity-${bottle._id}"><strong>Quantity: </strong> ${bottle.quantity}</span>
-                        <button class="btn btn-warning" onclick="DOMManager.deleteBottle('${order._id}', '${bottle._id}')">
-                        Delete Bottle</button>`
-                );
-            }
         }
     }
 }
 
 $('#create-new-order').click(() => {
-    DOMManager.createOrder($('#new-order-name').val());
+    DOMManager.createOrder($('#new-order-name').val(), $('#new-bottle-color').val(), $('#new-bottle-quantity').val());
+// Clear input boxes
     $('#new-order-name').val('');
+    $('#new-bottle-color').val('');
+    $('#new-bottle-quantity').val('');
 });
 
 DOMManager.getAllOrders();
